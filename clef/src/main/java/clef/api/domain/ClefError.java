8a4cc9447f513e98ecb00677247c74724eb73af9
Max DeCurtins <mdecurti@hsph.harvard.edu>
2018-06-24 16:41:50 -0400

    added fromStrings() method

--- clef/src/main/java/clef/api/domain/ClefError.java
+++ clef/src/main/java/clef/api/domain/ClefError.java
@@ -1,13 +1,38 @@
 package clef.api.domain;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ClefError extends ClefItem {
 
 	private String errorMsg;
 	
+	
+	/**
+	 * Create a ClefError instance with the given msg string.
+	 * 
+	 * @param msg
+	 */
 	public ClefError( String msg ) {
 		this.errorMsg = msg;
 	}
 	
+	
+	/**
+	 * Create a list of ClefError instances from a list of strings.
+	 * 
+	 * @param errs a list of String objects from which to create ClefErrors
+	 * @return a list of ClefError instances
+	 */
+	public static List<ClefError> fromStrings ( List<String> errs ) {
+		return errs.stream().map( s -> new ClefError(s) ).collect( Collectors.toList() );
+	}
+	
+	
+	/**
+	 * 
+	 * @return
+	 */
 	public String getMessage() {
 		return this.errorMsg;
 	}
