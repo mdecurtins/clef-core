1d958fe49b69a32e0ce0170ee38eb44b01d0b28a
Max DeCurtins <mdecurti@hsph.harvard.edu>
2018-07-19 22:36:37 -0400

    implemented setData() method

--- clef/src/main/java/clef/api/domain/response/DatasetProfileResponse.java
+++ clef/src/main/java/clef/api/domain/response/DatasetProfileResponse.java
@@ -1,15 +1,30 @@
 package clef.api.domain.response;
 
 import java.util.List;
+import java.util.ArrayList;
+
+import clef.mir.dataset.Dataset;
 
 public class DatasetProfileResponse extends ClefResponse {
 
+	private List<Dataset> datasets;
+	
 	public DatasetProfileResponse() {
 		super();
 		this.responseType = ResponseType.DATASET_PROFILE.toString();
+		this.datasets = new ArrayList<Dataset>();
+	}
+	
+	public List<Dataset> getDatasets() {
+		return datasets;
 	}
 	
 	public <T> void setData( List<T> l ) {
-		
+		for ( T o : l ) {
+			Dataset dset = (Dataset) o;
+			if ( ! this.datasets.contains( dset ) ) {
+				this.datasets.add( dset );
+			}
+		}
 	}
 }
