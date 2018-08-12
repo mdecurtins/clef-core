699710766039ab8dcc685a027e1f19f1e251bd53
Max DeCurtins <mdecurti@hsph.harvard.edu>
2018-08-12 17:24:05 -0400

    beginning metadata processing implementation

--- clef/src/main/java/clef/datamodel/metadata/MetadataServiceImpl.java
+++ clef/src/main/java/clef/datamodel/metadata/MetadataServiceImpl.java
@@ -8,10 +8,18 @@ import clef.datamodel.Tag;
 import clef.datamodel.TagRelation;
 import clef.datamodel.WorkType;
 import clef.datamodel.db.*;
+import clef.datamodel.metadata.parsers.Humdrum;
 import clef.mir.AlgorithmEnvironmentResponse;
+import clef.mir.MusicFormat;
 import clef.mir.dataset.Dataset;
+import clef.utility.CheckedFunction;
+import clef.utility.FileHandler;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class MetadataServiceImpl {
 
@@ -20,6 +28,23 @@ public class MetadataServiceImpl {
 		
 		
 		return null;
+	}
+	
+	/**
+	 * 
+	 * @param mf
+	 * @return
+	 */
+	private CheckedFunction<Path, Metadata> createFunctionForFormat( MusicFormat mf ) {
+		CheckedFunction<Path, Metadata> callback = null;
+		switch ( mf ) {
+		case HUMDRUM:
+			callback = Humdrum.getCreatorFunction();
+			break;
+		default:
+			callback = null;
+		}
+		return callback;
 	}
 	
 	public int insertComposer( Composer c ) {
@@ -51,9 +76,48 @@ public class MetadataServiceImpl {
 		return db.insert( ins );
 	}
 	
-	public void populateMetadata( Dataset dset, List<Metadata> dsetFiles ) {
-		// Iterate through files
-		// Parse metadata
-		// Store in database
+	
+	/**
+	 * 
+	 * @param dsets
+	 * @throws IOException
+	 */
+	public void populateMetadata( List<Dataset> dsets ) throws IOException {
+		
+		for ( Dataset d : dsets ) {
+			
+			MusicFormat mf = MusicFormat.valueOf( d.getDatasetAttributes().getFormat() );
+			
+			Predicate<Path> predicate = this.predicateForFormat( mf );
+			CheckedFunction<Path, Metadata> creatorFunction = this.createFunctionForFormat( mf );
+			
+			List<Metadata> meta = new ArrayList<Metadata>();
+			// Start gathering metadata from files in the directory where the clefdataset.json file resides.
+			if ( ! d.getParentDirectory().equals( "" ) ) {
+				meta = FileHandler.traversePath( d.getParentDirectory(), predicate, creatorFunction );
+			}
+			
+			if ( ! meta.isEmpty() ) {
+				
+			}
+		}
+		
+	}
+	
+	/**
+	 * 
+	 * @param mf
+	 * @return
+	 */
+	private Predicate<Path> predicateForFormat( MusicFormat mf ) {
+		Predicate<Path> pred = null;
+		switch ( mf ) {
+		case HUMDRUM:
+			pred = Humdrum.getPredicate();
+			break;
+		default:
+			pred = null;
+		}
+		return pred;
 	}
 }
