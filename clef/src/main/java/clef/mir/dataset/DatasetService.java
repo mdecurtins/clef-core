d5ff1220180d476c3994e1ec0aa04bd6b6f44510
Max DeCurtins <mdecurti@hsph.harvard.edu>
2018-08-12 16:02:03 -0400

    removed unused import, added javadoc

--- clef/src/main/java/clef/mir/dataset/DatasetService.java
+++ clef/src/main/java/clef/mir/dataset/DatasetService.java
@@ -1,27 +1,33 @@
 package clef.mir.dataset;
 
-import java.nio.file.Path;
 import java.util.List;
 
 import clef.common.ClefException;
 
 /**
+ * Simple interface for dataset operations.
  * 
  * @author Max DeCurtins
- *
+ * @since 1.0.0
  */
 public interface DatasetService {
 
+	
 	/**
+	 * Gets a list of all datasets stored by the DatasetService.
 	 * 
-	 * @return
+	 * @since 1.0.0
+	 * @return a list of all datasets available, empty if none found.
 	 */
 	public List<Dataset> getAllDatasets();
 	
+	
 	/**
+	 * Gets a dataset by name as specified in its clefdataset.json file. 
 	 * 
-	 * @param name
-	 * @return
+	 * @since 1.0.0
+	 * @param name the name of the desired dataset, as specified in its clefdataset.json file
+	 * @return the Dataset if successful, null if not found
 	 * @throws ClefException
 	 */
 	public Dataset getDataset( String name ) throws ClefException;
@@ -29,8 +35,10 @@ public interface DatasetService {
 
 	
 	/**
+	 * Lists the names of all available datasets.
 	 * 
-	 * @return
+	 * @since 1.0.0
+	 * @return a list of dataset names, empty if none found.
 	 */
 	public List<String> listDatasetNames();
 	
