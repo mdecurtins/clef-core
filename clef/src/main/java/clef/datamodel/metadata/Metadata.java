2637f7e1fdbe394b82d99374260d8cd0ea6e9d65
Max DeCurtins <mdecurti@hsph.harvard.edu>
2018-08-13 10:24:03 -0400

    added bipredicates for testing property values

--- clef/src/main/java/clef/datamodel/metadata/Metadata.java
+++ clef/src/main/java/clef/datamodel/metadata/Metadata.java
@@ -1,5 +1,7 @@
 package clef.datamodel.metadata;
 
+import java.util.function.BiPredicate;
+
 import clef.datamodel.*;
 
 /**
@@ -16,6 +18,17 @@ public class Metadata {
 	private Era era;
 	private Work w;
 	private WorkType wt;
+	
+	// Predicates
+	private static BiPredicate<Composer, Metadata> matchComposer = ( composer, m ) -> composer.getName().equals( m.getComposer().getName() );
+	private static BiPredicate<Era, Metadata> matchEra = ( era, m ) -> era.getValue().equals( m.getEra().getValue() ); 
+	private static BiPredicate<Work, Metadata> matchWork = ( w, m ) -> w.getTitle().equals( m.getWork().getTitle() ) &&
+																	   w.getCatalog().equals( m.getWork().getCatalog() ) && 
+																	   w.getCatalogNumber().equals( m.getWork().getCatalogNumber() );
+	private static BiPredicate<WorkType, Metadata> matchWorkType = ( wt, m ) -> wt.getValue().equals( m.getWorkType().getValue() );
+	
+	// Functions
+	
 	
 	public Composer getComposer() {
 		return c;
