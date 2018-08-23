b15e8480232dd00f4d9369ed9a6f69d5652b4a7f
Max DeCurtins <mdecurti@hsph.harvard.edu>
2018-08-23 14:31:10 -0400

    calling stored proc, added javadoc

--- clef/src/main/java/clef/datamodel/metadata/MetadataServiceImpl.java
+++ clef/src/main/java/clef/datamodel/metadata/MetadataServiceImpl.java
@@ -1,6 +1,8 @@
 package clef.datamodel.metadata;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiPredicate;
 import java.util.stream.Collectors;
 
 import clef.api.domain.ClefResult;
@@ -8,40 +10,32 @@ import clef.datamodel.db.ClefResultDAO;
 import clef.mir.AlgorithmEnvironmentResponse;
 import clef.mir.Result;
 
+/**
+ * Class that provides metadata for symbolic music query results.
+ * 
+ * @author Max DeCurtins
+ * @since 1.0.0
+ */
 public class MetadataServiceImpl implements MetadataService {
 
+	private BiPredicate<Result, Metadata> matchMeta = ( r, m ) -> r.getDatasetName().equals( m.getDatasetContent().getDatasetName() ) &&
+			r.getFilename().equals( m.getDatasetContent().getFilename() );
 
 	/**
-	 * Gets a string of escaped string values for a SQL IN( 'foo',...n ) clause.
+	 * Maps algorithm results contained in an instance of {@link AlgorithmEnvironmentResponse} to instances of {@link ClefResult}.
+	 *
+	 * This method uses the dataset and file names returned in the results from the algorithm to invoke a 
+	 * stored procedure that retrieves metadata.
 	 * 
-	 * A little hack-y, but sometimes that's the way it is. {@code values} is expected to be a list of 
-	 * filenames or dataset names, or other strings that should not contain escape characters like ""
-	 * 
-	 * @param values a list of strings to quote for a SQL IN clause
+	 * @param aer the response returned from a music information retrieval algorithm
 	 * @return
-	 * @since 1.0.0
-	 */
-	private String getEscapedStringInArray( List<String> values ) {
-		
-		StringBuilder sb = new StringBuilder();
-		boolean first = true;
-		for ( String s : values ) {
-			if ( first == true ) {
-				sb.append( "'" + s + "'" );
-				first = false;
-			}
-			sb.append( ",'" + s + "'" );
-		}
-		return sb.toString();
-	}
-
-	/**
-	 * 
-	 * @param aer
-	 * @return
+	 * @see ClefResultDAO#getJoinedMetadata(String, String)
+	 * @see clef.common.ClefService#doSearch(java.util.Map, String)
 	 * @since 1.0.0
 	 */
 	public List<ClefResult> mapMetadata( AlgorithmEnvironmentResponse aer ) {
+		
+		List<ClefResult> cres = new ArrayList<ClefResult>();
 		
 		// Gather dataset names and file names from AER results.
 		List<Result> results = aer.getResults();
@@ -49,14 +43,29 @@ public class MetadataServiceImpl implements MetadataService {
 		List<String> resultDatasetNames = results.stream().map( r -> r.getDatasetName() ).collect( Collectors.toList() );
 		List<String> resultFilenames = results.stream().map( r -> r.getFilename() ).collect( Collectors.toList() );
 		
-		// Call stored proc with these values. Gets metadata where dataset IN list of names AND filename IN list of filenames.
-		String datasetIN = this.getEscapedStringInArray( resultDatasetNames );
-		String filenameIN = this.getEscapedStringInArray( resultFilenames );
+		// Transform the collected result dataset names and filenames into CSV strings.
+		String datasetIN = String.join( ",", resultDatasetNames );
+		String filenameIN = String.join( ",", resultFilenames );
 		
+		// Call stored proc with these values. The proc will convert CSV string parameters into temp tables for use in subqueries.
 		ClefResultDAO crd = new ClefResultDAO();
 		List<Metadata> meta = crd.getJoinedMetadata( datasetIN, filenameIN );
-		// Make instances of ClefResult from ResultSet values, using composition of datamodel objects
 		
-		return null;
+		
+		// Merge metadata into results
+		for ( Result r : results ) {
+			ClefResult cr = new ClefResult();
+			for ( Metadata m : meta ) {
+				if ( this.matchMeta.test( r, m ) ) {
+					// We only need some of the properties that Metadata carries.
+					cr.setRanking( r.getID() );
+					cr.setComposer( m.getComposer() );
+					cr.setWork( m.getWork() );
+				}
+			}
+			cres.add( cr );
+		}
+		
+		return cres;
 	}
 }
