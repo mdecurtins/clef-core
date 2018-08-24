2ca16202a8d89015dbbd5a87d71f8d63f97077e2
Max DeCurtins <mdecurti@hsph.harvard.edu>
2018-08-23 20:43:44 -0400

    starting tag relation

--- clef/src/main/java/clef/datamodel/db/TagRelationDAO.java
+++ clef/src/main/java/clef/datamodel/db/TagRelationDAO.java
@@ -1,5 +1,13 @@
 package clef.datamodel.db;
 
+import java.util.List;
+
+import clef.datamodel.metadata.Metadata;
+
 public class TagRelationDAO extends ClefDAO {
 
+	public int batchInsert( List<Metadata> meta ) {
+		String sql = "INSERT INTO tag_relations ( work_id, tag_id ) VALUES ( ?, ? );";
+		return 0;
+	}
 }
