1dea90c607fcd438e20745c2a07eb3d663394c7f
Max DeCurtins <mdecurti@hsph.harvard.edu>
2018-08-24 15:51:09 -0400

    added javadoc

--- clef/src/main/java/clef/utility/CheckedFunction.java
+++ clef/src/main/java/clef/utility/CheckedFunction.java
@@ -2,6 +2,16 @@ package clef.utility;
 
 import java.io.IOException;
 
+/**
+ * Functional interface declaring that the apply() method may throw an IOException.
+ * 
+ * @author Max DeCurtins
+ *
+ * @param <T>
+ * @param <R>
+ * 
+ * @since 1.0.0
+ */
 @FunctionalInterface
 public interface CheckedFunction<T, R> {
 	R apply( T t ) throws IOException;
