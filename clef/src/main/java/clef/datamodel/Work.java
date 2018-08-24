85d526c1e7da326e4988e463ef1987e9fa40ce22
Max DeCurtins <mdecurti@hsph.harvard.edu>
2018-08-24 15:44:07 -0400

    added id properties for foreign key support

--- clef/src/main/java/clef/datamodel/Work.java
+++ clef/src/main/java/clef/datamodel/Work.java
@@ -1,18 +1,26 @@
 package clef.datamodel;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Work extends ClefDataObject {
 
+	@JsonIgnore
+	private int id;
 	private String catalog;
 	private String catalog_number;
+	@JsonIgnore
 	private int composerId;
 	private String composition_date;
 	private String era;
+	@JsonIgnore
 	private int eraId;
 	private String title;
 	private String type;
+	
+	@JsonIgnore
 	private int workTypeId;
 	
 	public Work() {
@@ -35,6 +43,7 @@ public class Work extends ClefDataObject {
 		return composition_date;
 	}
 	
+	@JsonIgnore
 	public int getComposerId() {
 		return composerId;
 	}
@@ -43,8 +52,14 @@ public class Work extends ClefDataObject {
 		return era;
 	}
 	
+	@JsonIgnore
 	public int getEraId() {
 		return eraId;
+	}
+	
+	@JsonIgnore
+	public int getId() {
+		return id;
 	}
 	
 	public String getTitle() {
@@ -55,6 +70,7 @@ public class Work extends ClefDataObject {
 		return type;
 	}
 	
+	@JsonIgnore
 	public int getWorkTypeId() {
 		return workTypeId;
 	}
@@ -62,9 +78,15 @@ public class Work extends ClefDataObject {
 	public void setCatalog( String c ) {
 		this.catalog = c;
 	}
+
 	
 	public void setCatalogNumber( String cn ) {
 		this.catalog_number = cn;
+	}
+
+	@JsonProperty
+	public void setComposerId( int id ) {
+		this.composerId = id;
 	}
 	
 	public void setCompositionDate( String cd ) {
@@ -75,12 +97,27 @@ public class Work extends ClefDataObject {
 		this.era = e;
 	}
 	
+	@JsonProperty
+	public void setEraId( int id ) {
+		this.eraId = id;
+	}
+	
+	@JsonProperty
+	public void setId( int id ) {
+		this.id = id;
+	}
+	
 	public void setTitle( String t ) {
 		this.title = t;
 	}
 	
 	public void setType( String ty ) {
 		this.type = ty;
+	}
+	
+	@JsonProperty
+	public void setWorkTypeId( int id ) {
+		this.workTypeId = id;
 	}
 	
 }
