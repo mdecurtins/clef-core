a02abaa82d4d19cabbae6452be3801b6df2531f9
Max DeCurtins <mdecurti@hsph.harvard.edu>
2018-08-24 15:43:34 -0400

    added id property, constructor, getter and setter

--- clef/src/main/java/clef/datamodel/Tag.java
+++ clef/src/main/java/clef/datamodel/Tag.java
@@ -1,16 +1,34 @@
 package clef.datamodel;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Tag extends Single {
 
+	@JsonIgnore
+	private int id;
 	private String tag;
 	
 	public Tag( String t ) {
 		this.tag = t;
 	}
 	
+	public Tag( int id, String t ) {
+		this.id = id;
+		this.tag = t;
+	}
+	
+	@JsonIgnore
+	public int getId() {
+		return id;
+	}
+	
 	public String getValue() {
 		return tag;
 	}
 	
-
+	@JsonProperty
+	public void setId( int id ) {
+		this.id = id;
+	}
 }
