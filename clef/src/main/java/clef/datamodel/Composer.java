f167fcd190863900f41e2e7b141f6d8770cbba8f
Max DeCurtins <mdecurti@hsph.harvard.edu>
2018-08-23 20:58:16 -0400

    added support for integer dates

--- clef/src/main/java/clef/datamodel/Composer.java
+++ clef/src/main/java/clef/datamodel/Composer.java
@@ -1,45 +1,74 @@
 package clef.datamodel;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.HashMap;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Composer extends ClefDataObject {
 
+	@JsonIgnore
+	private int id;
 	private String name;
-	private Map<String, String> dates;
+	private Map<String, Integer> dates;
+	
+	@JsonIgnore
+	private static final Logger logger = LoggerFactory.getLogger( Composer.class );
 	
 	public Composer() {
-		this.dates = new HashMap<String, String>();
+		this.dates = new HashMap<String, Integer>();
 	}
 	
 	public Composer( String name ) {
 		this.name = name;
-		this.dates = new HashMap<String, String>();
+		this.dates = new HashMap<String, Integer>();
 	}
 	
 	public Composer( String name, String born, String died ) {
 		this.name = name;
-		this.dates = new HashMap<String, String>();
+		this.dates = new HashMap<String, Integer>();
+		this.setDates( born, died );
+	}
+	
+	public Composer( int id, String name, String born, String died ) {
+		this.id = id;
+		this.name = name;
+		this.dates = new HashMap<String, Integer>();
+		this.setDates( born, died );
+	}
+	
+	public Composer( int id, String name, int born, int died ) {
+		this.id = id;
+		this.name = name;
+		this.dates = new HashMap<String, Integer>();
 		this.setDates( born, died );
 	}
 	
 	public int born() {
-		String born = this.dates.get( "born" );
+		Integer born = this.dates.get( "born" );
 		if ( born != null ) {
-			return Integer.parseInt( born );
+			return born;
 		}
 		return -1;
 	}
 	
 	public int died() {
-		String died = this.dates.get( "died" );
+		Integer died = this.dates.get( "died" );
 		if ( died != null ) {
-			return Integer.parseInt( died );
+			return died;
 		}
 		return -1;
+	}
+	
+	@JsonIgnore
+	public int getId() {
+		return id;
 	}
 	
 	public String getName() {
@@ -47,11 +76,18 @@ public class Composer extends ClefDataObject {
 	}
 	
 	
-	public Map<String, String> getDates() {
+	public Map<String, Integer> getDates() {
 		return dates;
 	}
 	
 	public void setDates( String born, String died ) {
+		Integer b = Integer.parseInt( born );
+		Integer d = Integer.parseInt( died );
+		this.dates.put( "born", b );
+		this.dates.put( "died", d );
+	}
+	
+	public void setDates( int born, int died ) {
 		this.dates.put( "born", born );
 		this.dates.put( "died", died );
 	}
