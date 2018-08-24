b957bea4d22236b14bddcb96d97c0f60872cdee1
Max DeCurtins <mdecurti@hsph.harvard.edu>
2018-08-23 20:51:11 -0400

    added javadoc

--- clef/src/main/java/clef/mir/MusicFormat.java
+++ clef/src/main/java/clef/mir/MusicFormat.java
@@ -1,5 +1,13 @@
 package clef.mir;
 
+/**
+ * Enum for symbolic music formats.
+ * 
+ * JSON is present as a format because Flat.io supports getting music as JSON data.
+ * 
+ * @author Max DeCurtins
+ * @since 1.0.0
+ */
 public enum MusicFormat {
 	HUMDRUM ( "humdrum" ),
 	JSON ( "json" ),
