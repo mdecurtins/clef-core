4d284ed48a2f1901713038330291a24c6c123546
Max DeCurtins <mdecurti@hsph.harvard.edu>
2018-08-24 15:42:57 -0400

    added id property, constructor, and id getter

--- clef/src/main/java/clef/datamodel/Era.java
+++ clef/src/main/java/clef/datamodel/Era.java
@@ -1,11 +1,25 @@
 package clef.datamodel;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public class Era extends Single {
 
+	@JsonIgnore
+	private int id;
 	private String era;
 	
 	public Era( String era ) {
 		this.era = era;
+	}
+	
+	public Era( int id, String era ) {
+		this.id = id;
+		this.era = era;
+	}
+	
+	@JsonIgnore
+	public int getId() {
+		return id;
 	}
 	
 	public String getValue() {
