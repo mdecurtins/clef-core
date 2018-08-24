8f9d39e9800085996ff377f70ba4fb48a7e70e24
Max DeCurtins <mdecurti@hsph.harvard.edu>
2018-08-24 16:18:09 -0400

    added javadoc, setters

--- clef/src/main/java/clef/datamodel/TagRelation.java
+++ clef/src/main/java/clef/datamodel/TagRelation.java
@@ -1,5 +1,14 @@
 package clef.datamodel;
 
+/**
+ * Class that represents the relationship between a tag and a musical work.
+ * 
+ * In this representation, the tag is the 'primary' object, and the work is the 
+ * object to which it's related.
+ * 
+ * @author Max DeCurtins
+ * @since 1.0.0
+ */
 public class TagRelation extends Relation {
 
 	private int tagId;
@@ -16,6 +25,14 @@ public class TagRelation extends Relation {
 	
 	public int getRelatedToId() {
 		return workId;
+	}
+	
+	public void setRelationId( int tagId ) {
+		this.tagId = tagId;
+	}
+	
+	public void setRelatedToId( int workId ) {
+		this.workId = workId;
 	}
 	
 }
