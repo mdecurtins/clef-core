f85687d3b8f7c24b8b9819e966a6a1fd22e3bfe4
Max DeCurtins <mdecurti@hsph.harvard.edu>
2018-08-24 15:45:05 -0400

    added id property, constructor, getter

--- clef/src/main/java/clef/datamodel/WorkType.java
+++ clef/src/main/java/clef/datamodel/WorkType.java
@@ -1,11 +1,25 @@
 package clef.datamodel;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public class WorkType extends Single {
 
+	@JsonIgnore
+	private int id;
 	private String workType;
 	
 	public WorkType( String wt ) {
 		this.workType = wt;
+	}
+	
+	public WorkType( int id, String wt ) {
+		this.id = id;
+		this.workType = wt;
+	}
+	
+	@JsonIgnore
+	public int getId() {
+		return id;
 	}
 	
 	public String getValue() {
